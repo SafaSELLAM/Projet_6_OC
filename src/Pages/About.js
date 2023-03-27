975544b67a3fe0563cba311732f8e77fc8b4d176
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-27 14:32:06 +0200

    docs : ajout banners

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Banner from "../Components/Banner";
+import AboutBanner from "../assets/about_banner.jpg";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="About">
+      <div className="banner_about">
+        <Banner image={AboutBanner} alt="image bannière" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Banner from "../Components/Banner";
+import homeBanner from "../assets/home_banner.png";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="Home">
+      <div className="banner_home">
+        <Banner image={homeBanner} text="Chez vous, partout et ailleurs" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
