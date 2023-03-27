99fb3cbbb7114f23bd325c54fa9594c48efb2511
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-27 14:28:20 +0200

    docs : bannière footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import logo_footer from "../assets/logo-footer.svg";
+import "../styles/Footer.css";
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <img src={logo_footer} alt="Logo footer kasa " />
+      <div className="text_footer">@ 2020 Kasa. All rights reserved</div>
+    </div>
+  );
 };
 
 export default Footer;
