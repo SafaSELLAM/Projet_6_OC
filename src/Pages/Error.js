5a9485dce2d9f649e0786f5d4e7053a0190b38f5
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-27 15:03:36 +0200

    docs: page erreur style

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "../styles/Error.css";
 const Error = () => {
-  return <div>Erreur 404</div>;
+  return (
+    <div className="error_container">
+      <div className="text_error_container">
+        <p className="error_text">404</p>
+        <p className="msg_error">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+      </div>
+      <div className="link_error">
+        <NavLink to="/"> Retourner sur la page d'Accueil</NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
