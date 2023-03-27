71d96b1523c95eebf1cb18ce90aa6bc64902b511
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-27 10:47:01 +0200

    docs : ajout css Nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
 const Navigation = () => {
   return (
     <div className="navigation">
       <ul>
-        <NavLink to="/">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+        >
           <li>Accueil</li>
         </NavLink>
-        <NavLink to="/about">
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+        >
           <li>A Propos</li>
         </NavLink>
       </ul>
