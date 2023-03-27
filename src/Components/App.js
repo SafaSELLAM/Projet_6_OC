a8456c1dfc153887a576ae2ccd6112b0a7501a7d
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-27 10:46:17 +0200

    fix : routings

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import About from "../Pages/About";
 import Error from "../Pages/Error";
 import Home from "../Pages/Home";
 import Logements from "../Pages/Logements";
 import "../styles/App.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logements" element={<Logements />} />
+        <Route path="/Error" element={<Error />} />
         <Route path="*" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
