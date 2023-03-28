d082a665c4fb79bf6494bf83fdea03b49aa663ef
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-28 11:41:53 +0200

    docs : ajout composant card page accueil

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Banner from "../Components/Banner";
 import homeBanner from "../assets/home_banner.png";
+import Cards from "../Components/Cards";
+
 const Home = () => {
   return (
     <div className="Home">
       <div className="banner_home">
         <Banner image={homeBanner} text="Chez vous, partout et ailleurs" />
       </div>
+      <Cards />
     </div>
   );
 };
