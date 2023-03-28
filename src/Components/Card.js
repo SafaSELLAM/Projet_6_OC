0a3317ba28c998d8a07ff543103b57cd204b797b
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-28 11:41:35 +0200

    docs : création des cards logements

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/card.css";
+const Card = (props) => {
+  // destructuration de props pour item
+  const { item } = props;
+  return (
+    <div className="card">
+      <img src={item.cover} alt={item.title} />
+      <h2>{item.title}</h2>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/Components/Cards.js
@@ -0,0 +1,19 @@
+import Card from "./Card.js";
+import data from "../datas/data";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/cards.css";
+const Cards = () => {
+  return (
+    // crée une card pour chaque logement
+    <div className="cards">
+      {data.map((item) => (
+        <NavLink key={item.id} to={`Logement/${item.id}`}>
+          <Card item={item} />
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
