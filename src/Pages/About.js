ffedc5530bfc65633d70aab508d01f840ddae49d
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-29 12:45:02 +0200

    fix : import données

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Banner from "../Components/Banner";
 import AboutBanner from "../assets/about_banner.jpg";
-import datas from "../datas/data.js";
+import datas from "../datas/about_data.js";
 import Collapse from "../Components/Collapse";
 const About = () => {
   return (
-    <div className="About">
+    <div className="about">
       <div className="banner_about">
         <Banner image={AboutBanner} alt="image bannière" />
-        {/* {datas.map((abouts) => (
-          // <Collapse
-          //   key={abouts.id}
-          //   about={{ title: abouts.title, text: abouts.text, list: false }}
-          // />
-        ))} */}
+        {datas.map((abouts) => (
+          <Collapse
+            key={abouts.id}
+            about={{ title: abouts.title, text: abouts.text, list: false }}
+          />
+        ))}
       </div>
     </div>
   );
