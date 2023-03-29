567abefeb5a7091bbff28a4525d6f846fd432458
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-29 11:28:49 +0200

    docs : test ajout collapse

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Banner from "../Components/Banner";
 import AboutBanner from "../assets/about_banner.jpg";
-
+import datas from "../datas/data.js";
+import Collapse from "../Components/Collapse";
 const About = () => {
   return (
     <div className="About">
       <div className="banner_about">
         <Banner image={AboutBanner} alt="image bannière" />
+        {/* {datas.map((abouts) => (
+          // <Collapse
+          //   key={abouts.id}
+          //   about={{ title: abouts.title, text: abouts.text, list: false }}
+          // />
+        ))} */}
       </div>
     </div>
   );
