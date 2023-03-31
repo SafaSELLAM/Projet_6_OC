8c8e88e954fe953ba39ef8e927d78836eec4dafc
SafaSELLAM <safa.sellam@outlook.fr>
2023-03-31 09:55:42 +0200

    docs : routes dans composant

---
+++ src/Components/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../Pages/About";
+import Error from "../Pages/Error";
+import Home from "../Pages/Home";
+import Logements from "../Pages/Logements";
+const Router = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logements/:id" element={<Logements />} />
+        <Route path="/Error" element={<Error />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Router;
